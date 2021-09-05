25cca39b1a041a97f4fa4cb4a82beb9e4f623078
seyeon <seyoen@gmail.com>
2021-09-05 23:44:58 +0900

    message-form-UI

--- src/components/ChatPage/MainPanel/MessagesForm.js
+++ src/components/ChatPage/MainPanel/MessagesForm.js
@@ -1,7 +1,34 @@
 import React from "react";
+import Form from "react-bootstrap/Form";
+import ProgressBar from "react-bootstrap/ProgressBar";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function MessagesForm() {
-  return <div>폼</div>;
+  return (
+    <div>
+      <Form>
+        <Form.Group controlid="">
+          <Form.Control as="textarea" row={3}></Form.Control>
+        </Form.Group>
+      </Form>
+
+      <ProgressBar now={60} variant="warning" />
+
+      <Row>
+        <Col>
+          <button className="message-form-button" style={{ width: "100%" }}>
+            SEND
+          </button>
+        </Col>
+        <Col>
+          <button className="message-form-button" style={{ width: "100%" }}>
+            SEND
+          </button>
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
 export default MessagesForm;
