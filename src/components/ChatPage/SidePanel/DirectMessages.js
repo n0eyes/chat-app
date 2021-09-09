4be21dcaf192b8c84b3ce1d9353c26e343e2d0ff
seyeon <doll4586633@naver.com>
2021-09-09 22:42:02 +0900

    direct-messages-ui

--- src/components/ChatPage/SidePanel/DirectMessages.js
+++ src/components/ChatPage/SidePanel/DirectMessages.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
+import { FaRegSmile } from "react-icons/fa";
+export class DirectMessages extends Component {
+  renderDirectMessages = () => {
+    return (
+      <li
+        style={{ paddingLeft: 5, cursor: "pointer", borderRadius: "5px" }}
+      >{`# 세연`}</li>
+    );
+  };
 
-function DirectMessages() {
-  return <div>DM</div>;
+  render() {
+    return (
+      <div>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <FaRegSmile />
+          {`DIRECT MESSAGES (1)`}
+          <div />
+        </div>
+        <ul style={{ listStyleType: "none", padding: 0 }}>
+          {this.renderDirectMessages()}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default DirectMessages;
