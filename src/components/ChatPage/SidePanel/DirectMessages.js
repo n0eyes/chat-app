83abfab229d9e8a6a3c0cf561d0c3df955cde82d
seyeon <doll4586633@naver.com>
2021-09-09 23:57:49 +0900

    read-dm-user-list

--- src/components/ChatPage/SidePanel/DirectMessages.js
+++ src/components/ChatPage/SidePanel/DirectMessages.js
@@ -1,15 +1,45 @@
 import React, { Component } from "react";
 import { FaRegSmile } from "react-icons/fa";
+import firebase from "firebase";
+import { connect } from "react-redux";
 export class DirectMessages extends Component {
+  state = {
+    usersRef: firebase.database().ref("user"),
+    users: [],
+    currentUser: this.props.currentUser,
+  };
+
+  componentDidMount() {
+    this.props.currentUser && this.addUsersListenrs(this.state.currentUser);
+  }
+  addUsersListenrs = (currentUser) => {
+    const { usersRef } = this.state;
+    const usersArray = [];
+    usersRef.on("child_added", (DataSnapshot) => {
+      const userInfo = DataSnapshot.val();
+      // 해당 data의 uid가 key 프로퍼티에 저장되어있다.
+      userInfo.uid = DataSnapshot.key;
+      userInfo.status = "offline";
+      currentUser.displayName !== userInfo.name && usersArray.push(userInfo);
+    });
+    this.setState({ users: usersArray });
+  };
+
   renderDirectMessages = () => {
-    return (
-      <li
-        style={{ paddingLeft: 5, cursor: "pointer", borderRadius: "5px" }}
-      >{`# 세연`}</li>
-    );
+    if (this.state.users.length > 0)
+      return this.state.users.map((user) => {
+        return (
+          <li
+            style={{ paddingLeft: 5, cursor: "pointer", borderRadius: "5px" }}
+            key={user.uid}
+          >{`# ${user.name}`}</li>
+        );
+      });
   };
 
   render() {
+    console.log("render-users", this.state.users);
+    console.log("current-users", this.state.currentUser);
     return (
       <div>
         <div
@@ -30,5 +60,9 @@ export class DirectMessages extends Component {
     );
   }
 }
-
-export default DirectMessages;
+const mapStateToProps = (state) => {
+  return {
+    currentUser: state.user.currentUser,
+  };
+};
+export default connect(mapStateToProps)(DirectMessages);
